eac1b9eebf4465ca801474de78813e467d2295b5
jaydip.sonani@cloudusinfotech.com <harshsuhagiya.cloudus22@gmail.com>
2024-12-02 09:46:13 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log("Rendering Component:", Component.name);
+  console.log("Page Props:", pageProps);
+
   return (
     <>
       <Component {...pageProps} />
